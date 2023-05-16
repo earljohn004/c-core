2ae0869efb6bb45660e98aeeba7310163f575eb0
abaquie <earl.john.abaquita@autodesk.com>
2023-05-16 14:01:26 +0800

    add openssl3 setup

---
+++ openssl3-setup.sh
@@ -0,0 +1,14 @@
+#/bin/bash
+
+ENV=$(PWD)
+
+rm -rf openssl-build/ && \
+wget https://www.openssl.org/source/openssl-3.1.0.tar.gz && \
+mkdir openssl-build && \
+tar xvf openssl-1.1.1t.tar.gz -C openssl-build --strip-components 1 && \
+cd openssl-build/ && \
+mkdir ./build/ && \
+./config --prefix=$ENV/openssl-build/build/ --openssldir=$ENV/openssl-build/build/ -Wl,-rpath,$ENV/openssl-build/build/ &&\
+make && make install && \
+cd .. && \
+make -C cpp -f posix_openssl.mk
