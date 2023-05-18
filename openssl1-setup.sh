213877c9d24ef5ec6289219e15fde301a7462c4d
abaquie <earl.john.abaquita@autodesk.com>
2023-05-18 16:54:26 +0800

    improve build script not to include docs

--- openssl1-setup.sh
+++ openssl1-setup.sh
@@ -9,6 +9,6 @@ tar xvf openssl-1.1.1t.tar.gz -C openssl-build --strip-components 1 && \
 cd openssl-build/ && \
 mkdir ./build/ && \
 ./config --prefix=$ENV/openssl-build/build/ --openssldir=$ENV/openssl-build/build/ -Wl,-rpath,$ENV/openssl-build/build/ &&\
-make && make install && \
+make && make install_sw install_ssldirs && \
 cd .. && \
 USE_OPENSSL=1 PUBNUB_CRYPTO_API=1  USE_GZIP_COMPRESSION=0 RECEIVE_GZIP_RESPONSE=0 USE_SUBSCRIBE_V2=1 USE_ADVANCED_HISTORY=1 USE_OBJECTS_API=0 USE_ACTIONS_API=0 USE_AUTO_HEARTBEAT=0 USE_GRANT_TOKEN=1 USE_REVOKE_TOKEN=0 USE_DNS_SERVERS=0 USE_PROXY=0 ONLY_PUBSUB_API=0 PUBNUB_RAND_INIT_VECTOR=1 make -C cpp -f posix_openssl.mk
